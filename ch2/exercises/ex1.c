2bf6dcd053012e6ebd5871c54269e095a4a3c736
Paul Ingles <paul@oobaloo.co.uk>
2011-06-29 19:38:00 +0100

    Add exercise 1

---
+++ ch2/exercises/ex1.c
@@ -0,0 +1,11 @@
+#import <stdio.h>
+
+int main(void)
+{
+  printf("Hello, world\n");
+  return 0;
+}
+
+// $ gcc ch2/exercises/ex1.c -o ex1 -Wall
+// ch2/exercises/ex1.c: In function ‘main’:
+// ch2/exercises/ex1.c:6: warning: control reaches end of non-void function
