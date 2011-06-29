7dad7a4d7337f01fe3c772250c16a133a318209f
Paul Ingles <paul@oobaloo.co.uk>
2011-06-29 20:25:00 +0100

    Add exercise 9: which spaces are essential

---
+++ ch2/exercises/ex9.c
@@ -0,0 +1,2 @@
+#import <stdio.h>
+int main(void){int height,length,width,volume,weight;height=8;length=12;width=10;volume=height*length*width;weight=(volume+165)/166; printf("Dimensions: %dx%dx%d\n",length,width,height);printf("Volume (cubic inches): %d\n",volume);printf("Dimensional weight (pounds): %d\n",weight);return 0;}
