9d05c67aa485dae5c3a6d4e841bdcbad0d8809d6
Paul Ingles <paul@oobaloo.co.uk>
2011-06-29 19:42:42 +0100

    Add ex2

---
+++ ch2/exercises/ex2.c
@@ -0,0 +1,22 @@
+#import <stdio.h>
+
+int main(void)
+{
+  printf("Parkinson's Law:\nWork expands so as to ");
+  printf("fill the time\n");
+  printf("available for its completion.\n");
+  return 0;
+}
+
+/* a) Identify the directives and statements in this program
+   b) What output does the program produce?
+   
+   a) Directives: import, statements: printf and return
+    
+   
+   b) $ ./ex2
+   Parkinson's Law:
+   Work expands so as to fill the time
+   available for it's completion.
+   $ 
+*/
