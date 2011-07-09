39eead21f4dabf852a0fe5410b13225c8edb918e
Paul Ingles <paul@oobaloo.co.uk>
2011-07-09 22:25:21 +0100

    Add exercises 2 and 3

---
+++ ch7/exercises/ex2.c
@@ -0,0 +1,27 @@
+#import <stdio.h>
+
+int main(void)
+{
+  printf("%f\n", 010E2);
+  printf("%f\n", 21.1E+5);
+  printf("%f\n", 3.978e-2);
+
+  printf("\n");
+  
+  return 0;
+}
+
+// Which of the following are not legal constants in C? Classify each
+// legal constant as either integer or floating point?
+
+// a) 010E2
+// b) 32.1E+5
+// c) 0790
+// d) 100_000
+// e) 3.978e-2
+
+// a) Valid float
+// b) Valid float
+// c) Illegal
+// d) Illegal
+// e) Valid float
---
+++ ch7/exercises/ex3.c
@@ -0,0 +1,9 @@
+// Which of the following are not legal types in C?
+//
+// a) short unsigned int
+// b) short float
+// c) long double
+// d) unsigned long
+//
+// legal: a, c
+// illegal: b, d
